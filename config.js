ae1674c5c91ba2824c5abe3c1d10f6723687e234
doananhtan-design <doananhtan@gmail.com>
2026-09-24 14:37:10 +0700

    ss

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// TNTT Châu Thủy PWA v2.1.5
-window.TNTT_CONFIG={GAS_URL:'https://script.google.com/macros/s/AKfycbyAZ0BdDGRlEBVzNPlbtOlIAwwzRTYig-sbZiVe91LDtkt92O3G5VAzqrnouqigN4k/exec',APP_VERSION:'2.1.5'};
+// TNTT Châu Thủy PWA v2.1.6
+window.TNTT_CONFIG={GAS_URL:'https://script.google.com/macros/s/AKfycbyAZ0BdDGRlEBVzNPlbtOlIAwwzRTYig-sbZiVe91LDtkt92O3G5VAzqrnouqigN4k/exec',APP_VERSION:'2.1.6'};
