8ab51f5f630a54647006d5c22188503467f2e804
doananhtan-design <doananhtan@gmail.com>
2026-09-25 20:42:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
    Chỉ cần đổi APP_VERSION khi phát hành bản mới.
    Ví dụ: 3.2.0 -> 3.2.1 -> 3.3.0
    ========================================================= */
-var TNTT_APP_VERSION = '3.2.0';
+var TNTT_APP_VERSION = '3.2.1';
 var TNTT_APP_VERSION_LABEL = 'V' + TNTT_APP_VERSION;
